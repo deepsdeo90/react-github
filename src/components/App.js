9b842181112d1dcfd4c948266642d25c9b0bc7b2
deepsdeo90 <deepalideore123@gmail.com>
2018-07-20 17:39:18 -0700

    added jquery and fetched data for user

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
-import  ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
-
+import $ from 'jquery'
 import '../css/bootstrap.css';
 import '../css/style.css';
 
@@ -9,13 +9,28 @@ class App extends Component{
 	constructor(props){
 		super(props);
 		this.state={
-			username:'dipali',
+			username:'deepsdeo90',
 			userData:[],
 			userRepos:[],
 			perPage:5
 		}
 	}
-
+	getUserData(){
+		$.ajax({
+		  url: "https://api.github.com/users/"+this.state.username+'?client_id='+this.props.clientId+'&client_secret='+this.props.clientSecret,
+		  dataType: 'json',
+		  cache: false,
+		  success: function(data) {
+		    console.log(data);
+		  }.bind(this),
+		  error: function(xhr,status,err){
+		  	alert(err);
+		  }.bind(this)
+		});
+	}
+	componentDidMount(){
+		this.getUserData();
+	}
 	render(){
 		return(
 				<div>
